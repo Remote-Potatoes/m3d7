b523f2dc646ccde55a9cb767e2c3ac5a2fd575da
juan eugenio <leuviah77@gmail.com>
2021-11-02 22:49:40 +0100

    useEffect function succes. Fetching API randomuser succed, but it needs styling

--- lab-example/pages/index.js
+++ lab-example/pages/index.js
@@ -1,12 +1,46 @@
-import React from "react";
+/** @format */
+
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
+import axios from "axios";
 
 const url = "https://randomuser.me/api/";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Good luck</h1>
-    </div>
-  );
+	const [characters, setCharacters] = useState([]);
+
+	useEffect(() => {
+		getCharacters();
+	}, []);
+
+	function getCharacters() {
+		axios
+			.get(url)
+			.then((res) => {
+				setCharacters(res.data.results);
+			})
+			.catch((err) => {
+				return "error: ", err.res;
+			});
+	}
+
+	return (
+		<div className={styles.container}>
+			{characters.map((person) => {
+				return (
+					<div key={person.id.value}>
+						<img
+							className={styles.imgContainer}
+							src={person.picture.large}
+							alt=""
+						/>
+						<h3>
+							{person.name.title}. {person.name.first} {person.name.last}
+						</h3>
+						<p>solo </p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
